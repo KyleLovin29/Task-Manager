324a598fa4522cb79ed72b2990f005ed12de42d6
Kyle Lovin <ky1e1ovin4@gmail.com>
2022-03-22 20:21:20 -0600

    Restructored Task Manager files

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
-const EventEmitter = require('events');
-// const Logger = require('./logger');
+// const Logger = require('./middleware/logger');
 const express = require('express');
-const Joi = require('joi');
+const tasks = require('./routes/tasks');
 const app = express();
 // const logger = new Logger();
 
 app.use(express.json());
-
-const tasks = [];
+app.use('/api/tasks', tasks);
 
 // logger.on('logEvent', (arg) => {
 //     console.log('Sending log to task-log.txt', arg)
@@ -16,56 +14,5 @@ const tasks = [];
 
 // logger.taskLog('test\n');
 
-app.get('/api/tasks', (req, res) => {
-    if(tasks.length === 0) 
-        return res.send('You have no tasks.');    
-    res.send(tasks);
-});
-app.get('/api/tasks/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
-    res.send(currentTask);
-});
-
-app.post('/api/tasks', (req, res) => {
-    const { error } = validateTask(req.body);
-    if (error) 
-        return res.status(400).send(error.details[0].message); 
-
-    const task = {
-        id: tasks.length + 1,
-        task: req.body.task
-    };
-
-    tasks.push(task);
-    res.send(tasks);
-});
-
-app.put('/api/tasks/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
-
-    const { error } = validateTask(req.body);
-    if (error) 
-        return res.status(400).send(error.details[0].message);
-
-    currentTask.task = req.body.task;
-    res.send(tasks);
-});
-
-app.delete('/api/tasks/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
-
-    const index = tasks.indexOf(currentTask);
-    tasks.splice(index, 1);
-
-    res.send(tasks);
-});
-
-
-function validateTask(task) {
-    const JoiSchema = Joi.object({
-        task: Joi.string().min(2).required()
-    })
-    return JoiSchema.validate(task)
-}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}. . .`));
