c220fd2a30a1301bf366decc26dec69c1830fef1
Kyle Lovin <ky1e1ovin4@gmail.com>
2022-03-28 17:52:45 -0600

    Used a Timeout function to simulate database reading

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,54 +5,71 @@ const router = express.Router();
 const tasks = [];
 
 router.get('/', (req, res) => {
-    if(tasks.length === 0) 
-        return res.send('You have no tasks.');    
-    res.send(tasks);
+    setTimeout(() => {
+        if(tasks.length === 0) 
+            return res.send('You have no tasks.');    
+        res.send(tasks);
+    }, 500)
+    
 });
 router.get('/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
+    setTimeout(() => {
+        const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
     res.send(currentTask);
+    }, 500)    
 });
 
 router.post('/', (req, res) => {
-    const { error } = validateTask(req.body);
-    if (error) 
-        return res.status(400).send(error.details[0].message); 
+    setTimeout(() => {
+        const error = validateTask(req.body);
+        if (error) 
+            return res.status(400).send(error.details[0].message); 
 
-    const task = {
-        id: tasks.length + 1,
-        task: req.body.task
-    };
+        const task = {
+            id: tasks.length + 1,
+            task: req.body.task
+        };
 
-    tasks.push(task);
-    res.send(tasks);
+        tasks.push(task);
+        res.send(tasks);
+    }, 500)
+    
 });
 
 router.put('/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
+    setTimeout(() => {
+        const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
 
-    const { error } = validateTask(req.body);
-    if (error) 
-        return res.status(400).send(error.details[0].message);
+        const error = validateTask(req.body);
+        if (error) 
+            return res.status(400).send(error.details[0].message);
 
-    currentTask.task = req.body.task;
-    res.send(tasks);
+        currentTask.task = req.body.task;
+        res.send(tasks);
+    }, 500)
+    
 });
 
 router.delete('/:id', (req, res) => {
-    const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
+    setTimeout(() => {
+        const currentTask = tasks.find(c => c.id === parseInt(req.params.id));
 
-    const index = tasks.indexOf(currentTask);
-    tasks.splice(index, 1);
+        const index = tasks.indexOf(currentTask);
+        tasks.splice(index, 1);
 
-    res.send(tasks);
+        res.send(tasks);
+    }, 500);
+    
 });
 
 function validateTask(task) {
-    const JoiSchema = Joi.object({
-        task: Joi.string().min(2).required()
-    })
-    return JoiSchema.validate(task)
+    setTimeout(() => {
+        const JoiSchema = Joi.object({
+            task: Joi.string().min(2).required()
+        })
+        return JoiSchema.validate(task)
+    }, 500);
+    
 }
 
 module.exports = router;
